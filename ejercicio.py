654b766f83c6968511f8e8adc37d86fe7b34b1bf
Carmanu <manuelbaltazar008@gmail.com>
2025-03-15 22:22:59 -0400

    segundo commit

--- ejercicio.py
+++ ejercicio.py
@@ -9,3 +9,17 @@ while numero >= 0:
             numeros.append(numero)
     except ValueError:
         print("ERROR, INGRESE UN NUMERO ENTERO")
+
+#####     SEGUNDO PROBLEMA     #####
+lista = []
+
+for i in range(5):
+    numero = int(input("INGRESE LOS NUMEROS: "))
+    lista.append(numero)
+
+suma = sum(lista)
+promedio = suma / len(lista)
+
+print("LISTA DE NUMEROS:", lista)
+print("LA SUMA ES:", suma)
+print("EL PROMEDIO ES:", promedio)
